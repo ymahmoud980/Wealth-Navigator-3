2e3e3203ee508dec11e1a101d80c908af8e28103
Yasser Mahmoud <YMahmoud1980@gmail.com>
2025-09-11 13:40:56 +0000

    Whenever I try to sign up or sign in I get this error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
